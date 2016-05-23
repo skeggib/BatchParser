b08bda3360427a6303bd3b764663231a35bb3019
Sebastien Klasa - U468135 <sebastien.klasa@mpsa.com>
2016-05-23 15:14:41 +0200

    Added licence and BatchInstruction methods (getArgumentByName and indexOf)

--- src/BatchParser/BatchArgument.hpp
+++ src/BatchParser/BatchArgument.hpp
@@ -1,8 +1,33 @@
-#ifndef BATCHPARSER_BATCHARGUMENT_H
-#define BATCHPARSER_BATCHARGUMENT_H
+/*
+Copyright (c) 2016, Klasa Sebastien
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+*  Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+*  Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+*  Neither the name of Klasa Sebastien nor the
+   names of its contributors may be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY KLASA SEBASTIEN ''AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL KLASA SEBASTIEN BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#pragma once
 
 #include <string>
-
 using namespace std;
 
 
@@ -15,7 +40,6 @@ namespace BatchParser {
  * @details Argument d'une instruction represente par un nom et une valeur, le
  * nom et la valeur peuvent etre vides. Par exemple '-o /home/user/out.txt' :
  * le nom est '-o' et la valeur est '/home/user/out.txt'
- * 
  */
 class BatchArgument {
 
@@ -73,5 +97,3 @@ public:
 } // namespace BatchParser
 } // namespace skeggib
 } // namespace com
-
-#endif // BATCHPARSER_BATCHARGUMENT_H
--- tests/BatchParser/TestBatchInstruction.cpp
+++ tests/BatchParser/TestBatchInstruction.cpp
@@ -1,3 +1,30 @@
+/*
+Copyright (c) 2016, Klasa Sebastien
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+*  Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+*  Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+*  Neither the name of Klasa Sebastien nor the
+   names of its contributors may be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY KLASA SEBASTIEN ''AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL KLASA SEBASTIEN BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include "TestBatchInstruction.hpp"
 
 
@@ -76,6 +103,15 @@ void TestBatchInstruction::test_has() {
 		"blender file.blend -v -d \"/home/skeggib/\" --no-console");
 	TEST_ASSERT(inst.has("azerty") == false);
 	TEST_ASSERT(inst.has("file.blend -v -d") == true);
+}
+
+void TestBatchInstruction::test_indexOf() {
+	BatchInstruction inst(
+		"blender file.blend -v -d \"/home/skeggib/\" --no-console");
+	BatchArgument * arg = inst.getArgumentByName("-v");
+	TEST_ASSERT(inst.indexOf(arg) == 1);
+	arg = inst.getArgumentByName("-d");
+	TEST_ASSERT(inst.indexOf(arg) == 2);
 }
 
 } // namespace BatchParser
